2ad693825b7a87b7b68d3ceea58904a8af817efa
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-21 14:58:08 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,13 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+state = {
+  review: [] 
+}
+componentDidMount (){
+  fetch(URL).then(resp=> resp.json())
+  .then(json => this.setState({
+    reviews:json.results
+  }))
+}
