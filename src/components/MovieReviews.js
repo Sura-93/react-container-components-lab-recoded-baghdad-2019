77700d9bfd414cd856efde04ba6ab23b4bfd3096
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-21 14:50:40 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,8 @@
 // Code MovieReviews Here
+import React form 'react'
+
+let MovieReviews =({reviews}) => 
+<ul className="review-list">{review ? review.map(review => {
+return <li className='review'>{review.display_title : null} )</li> 
+  
+})} </ul>
